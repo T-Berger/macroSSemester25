18a8dbf94cf903e7795db3e884fd2f7c7fd95aed
T-Berger <32510636+T-Berger@users.noreply.github.com>
2025-06-04 11:05:35 +0200

    changed DOG.py

--- Dog.py
+++ Dog.py
@@ -1,13 +1,3 @@
 import datetime
 import Animal
 
-class Dog(Animal):
-    def __init__(self, name, age, breed):
-        super().__init__(name, age)
-        self.breed = breed # Objekt Attribut
-
-## DRY 
-# DON'T REPEAT YOURSELF
-
-## KISS
-# Keep it simple stupid 
